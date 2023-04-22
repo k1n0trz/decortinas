da87d8debbc9b6c601cd09effd82c8672883eea0
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-04-21 23:59:08 -0500

    fix static

--- decortinas/settings.py
+++ decortinas/settings.py
@@ -134,6 +134,13 @@ MEDIA_ROOT = "uploads"
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "productos/static/"),)
 
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
+
+django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
