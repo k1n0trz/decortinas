d304428b07676ffd3da96995b6808cb35a72fb97
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-04-23 16:19:28 -0500

    db fix

--- decortinas/settings.py
+++ decortinas/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     "default": {
         #"ENGINE": "django.db.backends.sqlite3",
         #"NAME": BASE_DIR / "db.sqlite3",
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'l3wrmvkmj4jotlbi',
         'USER': 'yhvrqwrot3piu21h',
         'PASSWORD': 'jdblynh6ntgev2up',
