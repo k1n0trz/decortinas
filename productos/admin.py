8ca1b398d52bb58db202d6ac304242c9f45285ed
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-05-05 10:44:43 -0500

    update pages

--- productos/admin.py
+++ productos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Asesor, Product
+from .models import Asesor, Product, Pagina
 
 # Register your models here.
 admin.site.register(Asesor)
 admin.site.register(Product)
+admin.site.register(Pagina)
--- productos/models.py
+++ productos/models.py
@@ -16,12 +16,12 @@ class S3ProductImage(S3Boto3Storage):
 class Product(models.Model): # Crea la tabla product en db
     # Elementos del header del producto
     productmetadesc = models.CharField(max_length=200, verbose_name="Meta descripción del Producto")
-    productkeywords = models.CharField(max_length=200, verbose_name="Palabras clave del Producto")
+    productkeywords = RichTextField(verbose_name="Palabras clave del Producto")
     productbanner = models.ImageField(upload_to='imagenes-decortinas/', verbose_name="Imagen Banner del Producto", default='/productos/static/img/default.jpg')
     productbannermov = models.ImageField(upload_to='imagenes-decortinas/', verbose_name="Imagen Banner del Producto en Móvil", default='/productos/static/img/default-mov.jpg')
     # Elementos OG del producto
     productogdesc = models.CharField(max_length=200, verbose_name="Descripción OG del Producto")
-    productogtitle = models.CharField(max_length=200, verbose_name="Titulo OG del Producto")
+    productogtitle =models.CharField(max_length=200, verbose_name="Metatítulo OG del Producto")
     productogurl = models.CharField(max_length=200, verbose_name="Url OG del Producto")
     productogimg = models.CharField(max_length=200, verbose_name="Url OG Microformato del Producto")
     productogurlsec = models.CharField(max_length=200, verbose_name="Url OG Segura del Producto")
@@ -34,7 +34,8 @@ class Product(models.Model): # Crea la tabla product en db
     productdescription = RichTextField()
     productimg = models.ImageField(upload_to='imagenes-decortinas/', verbose_name="Imagen principal del Producto")
     gallerya = models.ImageField(upload_to='imagenes-decortinas/', verbose_name="Imagen secundaria del Producto", blank=True)
-    galleryb = models.ImageField(upload_to='imagenes-decortinas/', verbose_name="Imagen final del Producto", blank=True)
+    galleryb = models.ImageField(upload_to='imagenes-decortinas/', verbose_name="Imagen adicional del Producto", blank=True)
+    galleryc = models.ImageField(upload_to='imagenes-decortinas/', verbose_name="Imagen final del Producto", blank=True)
     # productimg = models.ImageField(upload_to='static/img/uploads/', verbose_name="Imagen principal del Producto")
     asesor = models.ForeignKey(Asesor, on_delete=models.SET_NULL, null=True)
     productprice = models.IntegerField(verbose_name="Precio de venta", blank=True, null=True, editable=False)
@@ -47,15 +48,20 @@ class Product(models.Model): # Crea la tabla product en db
             self.productprice = self.productoldprice - (self.productoldprice * self.productdiscount / 100)
         super().save(*args, **kwargs)
 
-# class Product(models.Model):
-#     productname = models.CharField(max_length=200, verbose_name="Nombre del Producto")
-#     productoldprice = models.IntegerField(verbose_name="Precio normal")
-#     productprice = models.IntegerField(verbose_name="Precio de venta")
-#     productdiscount = models.IntegerField(verbose_name="Precio de venta")
-#     producturl = models.CharField(max_length=200, verbose_name="Url producto detalle")
-#     productdescription = RichTextField()
-#     productimg = models.ImageField(upload_to='static/img/uploads/', verbose_name="Imagen principal del producto")
-#     asesor = models.ForeignKey(Asesor, on_delete=models.SET_NULL, null=True)
+class Pagina(models.Model):
+    pagename = models.CharField(max_length=200, verbose_name="Nombre de la página")
+    pagemetatitle = models.CharField(max_length=200, verbose_name="Metatítulo(title) de la página")
+    pagetitle = models.CharField(max_length=200, verbose_name="Título(h1) de la página")
+    pageslogan = models.CharField(max_length=200, verbose_name="Slogan(h2) de la página")
+    pagemetadesc = models.CharField(max_length=200, verbose_name="Meta descripción de la página")
+    pagekeywords = RichTextField(verbose_name="Palabras clave de la página")
+    pagebanner = models.ImageField(upload_to='imagenes-decortinas/', verbose_name="Imagen Banner de la página", default='/productos/static/img/default.jpg')
+    pagebannermov = models.ImageField(upload_to='imagenes-decortinas/', verbose_name="Imagen Banner de la página en Móvil", default='/productos/static/img/default-mov.jpg')
+    pageogdesc = models.CharField(max_length=200, verbose_name="Descripción OG de la página")
+    pageogtitle = RichTextField(verbose_name="Palabras clave de la página")
+    pageogurl = models.CharField(max_length=200, verbose_name="Url OG de la página")
+    pageogimg = models.CharField(max_length=200, verbose_name="Url OG Microformato de la página")
+    pageogurlsec = models.CharField(max_length=200, verbose_name="Url OG Segura de la página")
 
-#     def __str__(self):
-#         return self.productname + ' - creado por - ' + self.asesor.name
+    def __str__(self):
+        return self.pagename
