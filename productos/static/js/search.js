2987dfeb317a231b38f6de6eac7078f9fa448a0e
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-05-09 14:00:39 -0500

    fix comments

--- productos/static/js/search.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('mySearchForm');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita que el formulario se envíe
-  const query = document.querySelector('input[type="search"]').value;
-  // Ejecuta la búsqueda con la consulta "query"
-  // ...
-});
